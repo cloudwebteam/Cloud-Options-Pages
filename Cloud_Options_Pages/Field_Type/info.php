e91ac7277c35f25fed9c7f5cceca6492e621da9a
Anthony Plopper <anthony@cloudwebteam.com>
2012-11-15 15:07:25 -0600

    Added "info" field type.

---
+++ Cloud_Options_Pages/Field_Type/info.php
@@ -0,0 +1,87 @@
+<?php 
+class info extends Field_Type {
+	protected $info ;
+	protected $size = 45; 
+	protected $field ;
+	protected $label ;
+	protected $attributes = array() ;
+
+	public static function create_field( $args ){
+		$field_type = __CLASS__;
+		$field = new $field_type( $args ); 
+	}
+
+	protected function __construct( $args ){		
+		parent::__construct( __CLASS__, $args ); 	
+	}
+
+	protected function get_field_html( $args ){
+		$field = '';
+		return $field;
+	}
+	protected function get_label($field_info){
+		$label = "<label for='".$field_info['prefix'] . $field_info['id'] . "' >" . $field_info['title'] ."</label>";
+		return $label;
+	}	
+	protected function get_description( $field_info ){
+		$description = isset( $field_info['description']) && $field_info['description'] !== '' ? '<div class="text-content">'.$field_info['description'] . '</div>' : '';
+		return $description;
+	}
+   /**
+	* LAYOUTS FOR THIS FIELD
+	*/
+	public function standard ( $args ){
+	
+		?>
+		<tr valign="top" <?php echo $this->attributes; ?>>
+			<th scope="row"><?php echo $this->label; ?></th>
+			<td <?php echo $this->attributes; ?>>			
+				<?php echo $this->description; ?>
+			</td>
+		</tr>
+		<?php
+	}
+	public function expandable( $args ){
+		$field_info = parent::get_field_info($args);
+			
+	}
+	public function custom( $args ){
+		$layout_details = $this->info['layout']; ?>
+		<div <?php echo $this->attributes; ?>>
+		<?php
+		switch ( $layout_details['label'] ){
+			case 'left' : ?>
+				<p><?php echo $this->label; ?><?php echo $this->field; ?></p>
+				<?php echo $this->description; ?>
+				
+				<?php 
+				break;
+				
+			case 'right' : ?>
+				<p><?php echo $this->label; ?><?php echo $this->field; ?></p>
+				<?php echo $this->description; ?>
+
+				<?php 
+				break;
+
+			case 'top' : ?>
+				<p><?php echo $this->label; ?></p>
+				<p><?php echo $this->field; ?></p>
+				<?php echo $this->description; ?>
+
+				<?php 
+				break;	
+			default : ?>
+				<p><?php echo $this->label; ?></p>
+				<p><?php echo $this->field; ?></p>
+				<?php echo $this->description; ?>
+
+				<?php 
+				break; 
+		}
+		?>
+		</div>
+		<?php
+	}
+	
+}
