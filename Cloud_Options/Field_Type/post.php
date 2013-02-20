7b6167e98d7faf22f2efb743b8d45f57c3eb4a40
Anthony Plopper <anthony@cloudwebteam.com>
2013-02-20 17:10:24 -0600

    Fixed bug regarding post field type on options pages

--- Cloud_Options/Field_Type/post.php
+++ Cloud_Options/Field_Type/post.php
@@ -96,7 +96,7 @@ class Cloud_Field_post extends Field_Type {
 			$preview_data = $preview_data ? '<div class="inner">'.$preview_data.'</div>' : '' ;
 			$current_data .= '<div class="preview">'.$preview_data.'</div>' ;
 		} else {
-			get_metabox_options( $args['top_level'], $args['subpage'], $args['section'], $args['field'] ) ;
+			get_metabox_options( $this->args['top_level'], $this->args['subpage'], $this->args['section'], $this->args['field'] ) ;
 		}	
 		return $current_data ;
 	}
