a4bed927862bd6ce14754028ec711814189d3b6e
Anthony Plopper <anthony@cloudwebteam.com>
2013-03-04 10:11:22 -0600

    Continued rewrite: implemented startend.

--- Cloud/Cloud_Forms/Field/_js/startend.js
+++ Cloud/Cloud_Forms/Field/_js/startend.js
@@ -1,71 +1,119 @@
 jQuery( function($){
 
-	$('.field.type-startend .datepicker').each( function(){
-		var field = $(this).parents( '.field.type-startend' );	
-		var is_start = $(this).hasClass( 'start' );
-			
-		var dateFormat = $(this).data('dateformat'); 	
-		var timeFormat = $(this).data('timeformat'); 
+	$('.field.type-startend').each( function(){
+		var field = $(this) ;	
 		
-		var end = field.find( 'input.end' ); 
-		var start = field.find( 'input.start' );	
+		var picker_type = field.find( '.selector' ).data('field_type' ); 
+		
+		var datepickers = field.find( '.datepicker' ) ; 
+		var timepickers = field.find( '.timepicker' ) ; 
+		
+		function convert_to_saved_value( input, combined_timestamp ){
 
-		var input = $(this) ;
-		if( input.val() ){
-			var startingValue = new Date( input.val()*1000 ) ;
-		} else {
-			var startingValue = false ;
-		}
-					
-		var onClose = function( time, instance ){
-			if ( time ){
-				var year = instance.selectedYear;
-				var month = instance.selectedMonth; 
-				var day = instance.selectedDay; 
-				var date_object = new Date( year, month, day ) ; 
-				
+			if ( combined_timestamp ){
+				var is_start = input.hasClass('.start' ); 
+				if ( is_start ){	
+					var timestamp_input = field.find( '.start .timestamp' );	
+					var timeFieldData = field.find( '.timepicker.start' ).data('timepicker' ); 										
+					var dateFieldData = field.find( '.datepicker.start' ).data('datepicker') ; 
+				} else {
+					var timestamp_input = field.find( '.end .timestamp' );				
+					var timeFieldData = field.find( '.timepicker.end' ).data('timepicker' ); 
+					var dateFieldData = field.find( '.datepicker.end' ).data('datepicker') ;
+				}		
+				var year = dateFieldData.selectedYear ; 
+				var month = dateFieldData.selectedMonth ; 					
+				var day = dateFieldData.selectedDay ; 
+				var hour = timeFieldData.hour ; 
+				var minute = timeFieldData.minute ; 
+				var date_object = new Date( year, month, day, hour, minute ) ; 
 				var timestamp = date_object.getTime() /1000 ;
 				var value_to_save = {
-					'datetime' : timestamp
+					'date' : timestamp
 				} ;
-				
-				input.siblings( '.timestamp' ).val( JSON.stringify( value_to_save ) ) ;			
-				if ( is_start ){
-					end.datepicker('option', 'minDate', start.datepicker( 'getDate' ) );
-				}
+			
+				timestamp_input.val( JSON.stringify( value_to_save ) ) ;	
 			} else {
-				input.siblings( '.timestamp' ).val( '' ) ;			
+				if ( is_start ){	
+					var timestamp_input = field.find( '.start .timestamp' );	
+				} else {
+					var timestamp_input = field.find( '.end .timestamp' );				
+				}			
+				timestamp_input.val( input.val() );
 			}
-		};
-
-		input.datepicker({
-			dateFormat : dateFormat,
-			timeFormat : timeFormat,
-			onClose : onClose
-		}); 
-		if ( startingValue ){
-			input.datepicker('setDate', startingValue )  ;
+	console.log( timestamp_input.val() );			
 		}
-				
-	}); 	
-	$('.field.type-startend .timepicker').each( function(){
-		var field = $(this).parents( '.field.type-startend' );	
-		var is_start = $(this).hasClass( 'start' );			
-
-		var timeFormat = $(this).data('timeformat'); 
+		datepickers.each( function(){
+			var input = $(this) ;
+			var is_start = input.hasClass( 'start' );		
+			var dateFormat = input.data('dateformat'); 	
 		
-		var is_start = $(this).hasClass( 'start' );
-		if ( is_start ){
-			var end = field.find( 'input.end' ); 
-			var start = field.find( 'input.start' );
-			var onChange = function( e ){
-				if (end.val() == ''  ) {
-					end.timepicker( 'setTime', e.time.value );
+			if( input.val() ){
+				var startingValue = new Date( input.val()*1000 ) ;
+			} else {
+				var startingValue = new Date() ;
+			}
+			var end = field.find( 'input.datepicker.end' ); 
+			var timestamp_input = input.siblings( '.timestamp' );		
+			var onClose = function( time, instance ){
+				if ( time ){
+					var year = instance.selectedYear;
+					var month = instance.selectedMonth; 
+					var day = instance.selectedDay; 
+					var date_object = new Date( year, month, day ) ; 
+					
+					var timestamp = date_object.getTime() /1000 ;
+					var value_to_save = {
+						'date' : timestamp
+					} ;
+					
+					timestamp_input.val( JSON.stringify( value_to_save ) ) ;			
+					if ( is_start ){
+						if ( end.val() ){
+							end.datepicker('option', 'minDate', input.datepicker( 'getDate' ) );
+						} else {
+							end.datepicker('setDate', input.datepicker( 'getDate' ) );
+						}
+					}
+				} else {
+					timestamp_input.val( '' ) ;			
 				}
 			};
-		} else {
-			var onChange = false ;
-		}
-		$(this).timepicker().on( 'changeTime.timepicker', onChange ) ; 
+			input.datepicker({
+				dateFormat : dateFormat,
+				onClose : onClose
+			}); 	
+			if ( startingValue ){
+				input.datepicker('setDate', startingValue )  ;
+			}				
+		}); 
+			
+		timepickers.each( function( e ){
+			var input = $(this);
+			var is_start = input.hasClass( 'start' );			
+
+			var end = field.find( '.timepicker.end' );
+			if ( picker_type !== 'time' ){
+				var onChange = function( e ){
+					convert_to_saved_value( input, true )
+					if ( is_start ){
+						if ( end.val() ){
+							end.datepicker('option', 'minDate', input.datepicker( 'getDate' ) ) ;
+						} else {
+							end.datepicker('setDate', input.datepicker( 'getDate' ) );
+						}
+						convert_to_saved_value( end, true );
+					}
+				}				
+			} else {
+				var onChange = function(){
+					convert_to_saved_value( input, false ) ;
+				} ;
+			}
+			$(this).timepicker().on( 'changeTime.timepicker' , onChange ) ; 
+		}); 	
+
+				
 	}); 	
+
 });
--- Cloud/Cloud_Forms/Field/startend.php
+++ Cloud/Cloud_Forms/Field/startend.php
@@ -4,18 +4,13 @@ defined( 'ABSPATH' ) || exit;
 
 class Cloud_Field_startend extends Cloud_Field {
 	public static function create_field( $args ){
-		$field_type = __CLASS__;
-		$field = new $field_type( $args ); 
-	}
-
-	protected function __construct( $args ){		
-		parent::__construct( __CLASS__, $args ); 	
+		$field = new self( $args ); 
 	}
 
 	protected function get_field_html( ){
 		$this->size = isset( $this->spec['size'] ) ? $this->spec['size'] : ''; 	
 
-		$field_type = isset( $this->spec['field_type'] ) ? $this->spec['field_type'] : 'date' ;
+		$this->field_type = isset( $this->spec['field_type'] ) ? $this->spec['field_type'] : 'date' ;
 		$this->date_format = isset( $this->spec['date_format'] ) ?  $this->spec['date_format'] : 'mm/dd/yy'  ;
 		$this->time_format = isset( $this->spec['time_format'] ) ?  $this->spec['time_format'] : 'hh:mm tt' ; 
 
@@ -26,59 +21,65 @@ class Cloud_Field_startend extends Cloud_Field {
 		
 		$this->start_utc_field = '<input class="timestamp" type="hidden" name="'.$this->info['name'] . '[start]" value=\'' . $this->start_value . '\' />';
 		$this->end_utc_field = '<input class="timestamp" type="hidden" name="'.$this->info['name'] . '[end]" value=\'' . $this->end_value . '\' />';
-	
-		switch( $field_type ){
 		
-			case 'time' : 
-				$fields = $this->get_time_fields( ) ;
-				break; 
-			case 'date' : 
-			default: 
-				$fields = $this->get_date_fields( ) ;			
-				break; 								
-		}
-		return $fields;
-	}
+
+		$field = $this->make_start_or_end_field( 'start' ); 
+		$field .= $this->make_start_or_end_field( 'end' ); 	
+		
+		return $field ;
+	}	
+
 	protected function parse_dynamic_value( $value ){
 		$json_array = json_decode( $value , true) ; 
 		return isset( $json_array['datetime'] ) ? $json_array['datetime'] : false ;	
 	}
-	protected function get_date_fields( ){
-	
-		$fields = '' ;
-		$fields .= '<span class="selector"><span class="start-label">Start</span>'.$this->start_utc_field.'<input data-dateformat="'.$this->date_format.'" type="text" id="' . $this->info['id'] . '" class="datepicker start" size="'.$this->size.'" value="' .  $this->start_utc. '" /></span>' ;
-		$fields .= '<span class="selector"><span class="end-label">End</span>'.$this->end_utc_field.'<input data-dateformat="'.$this->date_format.'" type="text" id="' . $this->info['id'] . '-end" class="datepicker end" size="'.$this->size.'" value="' .  $this->end_utc . '" /></span>';	
-
-		return $fields; 
+	protected function make_start_or_end_field( $start_or_end ){
+		$fields = '<span class="selector '.$start_or_end.'" data-field_type="' . $this->field_type. '" >'; 
+		switch( $this->field_type ){
+			case 'time' : 
+				$fields .= $this->get_sub_label( $start_or_end ); 			
+				$fields .= $this->start_utc_field;
+				$fields .= $this->get_time_field( $start_or_end ) ;	
+				break; 
+			case 'datetime' : 
+				$fields .= $this->get_sub_label( $start_or_end ); 						
+				$fields .= $this->start_utc_field;
+				$fields .= $this->get_date_field( $start_or_end ) ;
+				$fields .= $this->get_time_field( $start_or_end ) ;
+				break;
+			case 'date' : 
+			default: 
+				$fields .= $this->get_sub_label( $start_or_end );				
+				$fields .= $this->start_utc_field;
+				$fields .= $this->get_date_field( $start_or_end ) ;
+				break;
+		}
+		$fields .= '</span>'; 		
+		return $fields;
 	}
-	protected function get_time_fields( ){
+	protected function get_sub_label( $start_or_end ){
+		$label = '<span class="'.$start_or_end.'-label">'.ucfirst( $start_or_end ).'</span>' ;
+		return $label;
+	}
+	protected function get_date_field( $start_or_end ){
+		$field = '<input data-dateformat="'.$this->date_format.'" type="text" id="' . $this->info['id'] . '-'.$start_or_end . '" class="datepicker '.$start_or_end.'" size="'.$this->size.'" value="' .  $this->start_utc. '" />' ;
+		return $field ;
+	}
+	protected function get_time_field( $start_or_end ){
+		$field = '<div class="input input-append bootstrap-timepicker">' ; 
+        $field .= '<input name="'.$this->info['name'] . '['.$start_or_end.']" type="text" value="'.$this->start_value.'" class="timepicker '.$start_or_end.' input-small">' ;
+        $field .= '<span class="add-on"><i class="icon-time"></i></span>' ;
+        $field .= '</div>' ;	
+        return $field ;
+    }		
+	public function enqueue_scripts_and_styles(){
 
-		$fields = '' ;
-        $fields .= '<span class="start-label">Start</span>';
-        $fields .= '<div class="input input-append bootstrap-timepicker">' ; 
-        $fields .= '<input id="'. $this->info['id'].'" name="'.$this->info['name'] . '[start]" type="text" value="'.$this->start_value.'" class="timepicker start input-small">' ;
-        $fields .= '<span class="add-on"><i class="icon-time"></i></span>' ;
-        $fields .= '</div>' ;	
-        $fields .= '<span class="end-label">End</span>';
-        $fields .= '<div class="input input-append bootstrap-timepicker">' ; 
-        $fields .= '<input id="'. $this->info['id'].'-end" name="'.$this->info['name'] . '[end]" type="text" value="'.$this->end_value.'" class="timepicker end input-small">' ;
-        $fields .= '<span class="add-on"><i class="icon-time"></i></span>' ;
-        $fields .= '</div>' ;	        	
-	//	$fields .= '<span class="selector"><span class="start-label">Start</span>'.$this->start_utc_field.'<input data-dateformat="'.$this->date_format.'" type="text" id="'.$this->info['prefix'] . $this->info['id'] . '" class="timepicker start" size="'.$this->size.'" value="' .  $this->start_utc. '" /></span>' ;
-	//	$fields .= '<span class="selector"><span class="end-label">End</span>'.$this->end_utc_field.'<input data-dateformat="'.$this->date_format.'" type="text" id="'.$this->info['prefix'] . $this->info['id'] . '-end" class="timepicker end" size="'.$this->size.'" value="' .  $this->end_utc . '" /></span>';	
-
-		return $fields; 
-	}		
-	public function enqueue_field_scripts_and_styles(){
-
-		$this->enqueue_script( 'jquery-ui-datepicker' );
-		$this->enqueue_script( 'jquery-ui-slider' );
 		$this->enqueue_script( 'bootstrap-timepicker' );
-		
 		$this->enqueue_style( 'bootstrap-timepicker' );
+		
+		$this->enqueue_script( 'jquery-ui-datepicker' );
 
-		// if they exist, enqueues css and js files with this fields name
-		parent::register_scripts_and_styles( __CLASS__ ); 
+		parent::enqueue_scripts_and_styles( ); 
 	}	
 	
    /**
