bca370e3515d2e9c712452b37d66f81e82e52b66
Anthony Plopper <anthony@cloudwebteam.com>
2013-07-01 10:29:07 -0500

    Fixed a bug involving clones and trickiness.

--- Cloud/Cloud_Forms/Field/_js/date.js
+++ Cloud/Cloud_Forms/Field/_js/date.js
@@ -1,50 +1,54 @@
 CloudField.on( 'init', function( $context ){
 	var selector = '.field.type-date .datepicker'; 
-	var $fields = $( selector, $context ).add( $context.filter( selector ) ); 
+	var $inputs = $( selector, $context ).add( $context.filter( selector ) ); 
 	
-	$fields.each( function(){
-		var dateFormat = $(this).data('dateformat'); 	
-		var minDate = $(this).data( 'mindate' ); 
-		var maxDate = $(this).data( 'maxdate' ); 
+	$inputs.each( function(){
+	
+		if ( $(this).parents('.to-clone').size() == 0 ){
 			
-		var input = $(this) ;
-		if ( input.hasClass( 'saves-json') && input.val() ){
-			var startingValue = new Date( input.val()*1000 ) ;
-		} else if( input.val() ){
-			var startingValue = new Date( input.val() ) ;
-		} else {
-			var startingValue = '' ;
-		}
-		
-		input.datepicker({
-			dateFormat : dateFormat,
-			minDate : minDate,
-			maxDate : maxDate,
-			onClose : function( time, instance ){
-				if ( time ){			
-					var year = instance.selectedYear;
-					var month = instance.selectedMonth; 
-					var day = instance.selectedDay; 
-					var date_object = new Date( year, month, day ) ; 
-					
-					var timestamp = date_object.getTime() /1000 ;
-					var value_to_save = {
-						'datetime' : timestamp
-					} ;
-					input.siblings( '.timestamp' ).val( JSON.stringify( value_to_save ) ) ;
-				} else {
-					input.siblings( '.timestamp' ).val( '' ) ;
-				}	
-				input.blur();
-			}			
-		}); 
-		if ( typeof minDate !== 'undefined' ){
-			input.datepicker('option', 'minDate', minDate )  ;
-		} 
-		if ( typeof maxDate !== 'undefined' ){		
-			input.datepicker('option', 'maxDate', maxDate )  ;
-		}
-		input.datepicker('setDate',startingValue )  ;
-		
+			var $input = $(this) ;
+	
+			var dateFormat = $(this).data('dateformat'); 	
+			var minDate = $(this).data( 'mindate' ); 
+			var maxDate = $(this).data( 'maxdate' ); 
+				
+	
+			if ( $input.hasClass( 'saves-json') && input.val() ){
+				var startingValue = new Date( input.val()*1000 ) ;
+			} else if( $input.val() ){
+				var startingValue = new Date( input.val() ) ;
+			} else {
+				var startingValue = '' ;
+			}
+			$input.datepicker({
+				dateFormat : dateFormat,
+				minDate : minDate,
+				maxDate : maxDate,
+				onClose : function( time, instance ){
+					if ( time ){			
+						var year = instance.selectedYear;
+						var month = instance.selectedMonth; 
+						var day = instance.selectedDay; 
+						var date_object = new Date( year, month, day ) ; 
+						
+						var timestamp = date_object.getTime() /1000 ;
+						var value_to_save = {
+							'datetime' : timestamp
+						} ;
+						$input.siblings( '.timestamp' ).val( JSON.stringify( value_to_save ) ) ;
+					} else {
+						$input.siblings( '.timestamp' ).val( '' ) ;
+					}	
+					$input.blur();
+				}			
+			}); 
+			if ( typeof minDate !== 'undefined' ){
+				$input.datepicker('option', 'minDate', minDate )  ;
+			} 
+			if ( typeof maxDate !== 'undefined' ){		
+				$input.datepicker('option', 'maxDate', maxDate )  ;
+			}
+			$input.datepicker('setDate',startingValue )  ;
+		}	
 	}); 
 });
