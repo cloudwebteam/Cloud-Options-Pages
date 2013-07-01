90c5f93f0e26c8a576c9a8afaa8f1e3f7393cbfb
Anthony Plopper <anthony@cloudwebteam.com>
2013-07-01 10:24:22 -0500

    Added radio field.

---
+++ Cloud/Cloud_Forms/Field/radio.php
@@ -0,0 +1,27 @@
+<?php 
+// Prevent loading this file directly
+defined( 'Cloud_ABS' ) || exit;
+
+class Cloud_Field_radio extends Cloud_Field {
+
+	public static function create_field( $args ){
+		$field = new self( $args ); 
+	}
+
+	protected function get_field_html( ){
+		$field = '' ; 
+		foreach( $this->spec['options'] as $value => $text ){
+			$checked = $this->info['value'] == $value ? ' checked ' : '';		
+			$field .= '<div class="radio-group">'; 
+			$field .= '<input type="radio" id="'.$this->info['id'] . '-'.$value.'" name="'.$this->info['name'] . '" value="'.$value.'"' . $checked .' />';		
+			$field .= '<label for="'.$this->info['id'] . '-' . $value .'">'.$text.'</label>' ; 
+			$field .= '</div>'; 
+		}
+		return $field; 
+	}
+	
+   /**
+	* LAYOUTS FOR THIS FIELD
+	*/	
+}
+
