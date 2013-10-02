6088add5a508b57dec903e18fa3f9f0fe4df3cab
Anthony Plopper <anthony@cloudwebteam.com>
2013-10-02 14:23:39 -0500

    Fixed bug with timepicker loading its scripts.

--- Cloud/Cloud_Forms/Field/time.php
+++ Cloud/Cloud_Forms/Field/time.php
@@ -25,7 +25,7 @@ class Cloud_Field_time extends Cloud_Field {
 		self::enqueue_style( 'bootstrap-timepicker' );
 
 		// if they exist, enqueues css and js files with this fields name
-		parent::enqueue_scripts_and_styles( $field_type = false ); 
+		parent::enqueue_scripts_and_styles( $field_type ); 
 	}	
 	
    /**
